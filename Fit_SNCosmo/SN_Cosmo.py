38139fd4d2953473b3e517bc97823736e2f95df4
Philippe Gris <philippe.gris@clermont.in2p3.fr>
2018-10-12 10:24:23 +0200

    few adds

--- Fit_SNCosmo/SN_Cosmo.py
+++ Fit_SNCosmo/SN_Cosmo.py
@@ -6,6 +6,7 @@ class Fit_LC:
     def __init__(self,model='salt2-extended',version=1.0,telescope=None,display=False,bands='ugrizy'):
       
         self.display = display
+        self.bands = bands
 
         for band in bands:
             if telescope.airmass > 0:
@@ -26,11 +27,24 @@ class Fit_LC:
 
         select=lc[np.where(np.logical_and(lc['flux']/lc['fluxerr']>5.,lc['flux']>0.))]
         select = select[['flux','fluxerr','band','zp','zpsys','time']]
-        print(select)
-        res, fitted_model = sncosmo.fit_lc(select, self.SN_fit_model,['t0', 'x0', 'x1', 'c'],bounds={'z':(z-0.001, z+0.001)})
-        mbfit=fitted_model._source.peakmag('bessellb','vega')
+        
+        res_param_names = ['z', 't0', 'x0', 'x1', 'c']
+        res_params_values = np.zeros((5,), dtype=float)
+        vparam_names = ['t0', 'x0', 'x1', 'c']
+        covariance = np.zeros((4,4,), dtype=float)
+        mbfit = -1
+        
+        if len(select) > 0:
+            res, fitted_model = sncosmo.fit_lc(select, self.SN_fit_model,['t0', 'x0', 'x1', 'c'],bounds={'z':(z-0.001, z+0.001)})
+            mbfit=fitted_model._source.peakmag('bessellb','vega')
+            res_param_names = res['param_names']
+            res_params_values = res['parameters']
+            vparam_names = res['vparam_names']
+            covariance = res['covariance']
+            
 
-        #print('hello',self._transform(lc.meta,res['param_names'],list(res['parameters']),res['vparam_names'],res['covariance'],mbfit))
+        #print(type(res),type(fitted_model))
+        #print('hello',res,type(res['param_names']),res['parameters'].shape,type(res['vparam_names']),res['covariance'].shape)
         """
         print('res',res)
         print('fitted',fitted_model)
@@ -42,26 +56,59 @@ class Fit_LC:
             sncosmo.plot_lc(select, model=fitted_model,color='r',pulls=False,errors=res.errors) 
             plt.show()
 
-        return self._transform(lc.meta,res['param_names'],list(res['parameters']),res['vparam_names'],res['covariance'],mbfit)
+        resa = self._transform(lc.meta,res_param_names, list(res_params_values), vparam_names,covariance,mbfit)
+        resb = self._get_infos(z,res_params_values[res_param_names.index('t0')],select)
+
+        resa.update(resb)
+        
+        return resa.keys(), resa.values()
     
     def _transform(self,meta,par_names,params,vpar_names,covmat,mbfit):
-
-        rv = []
-        names = []
-        for key, value in meta.items(): 
-            names.append(key)
-            rv.append(value)
-
         
-        names += [par_names[i]+'_fit' for i in range(len(par_names))]
-        rv += params
+        res = {}
+        for key, value in meta.items(): 
+            res[key]=value
+
+        for i in range(len(par_names)):
+            res[par_names[i]+'_fit']=params[i]
 
         for i, name in enumerate(vpar_names):
             for j, nameb in enumerate(vpar_names):
                 if j >= i:
-                    names.append('Cov_'+name+nameb)
-                    rv.append(covmat[i,j])
-        names.append('mbfit')
-        rv.append(mbfit)
-        #print(names,rv)
-        return names, rv
+                    res['Cov_'+name+nameb] = covmat[i,j]
+                    
+        res['mbfit'] = mbfit
+        return res
+
+    def _get_infos(self, z, T0, lc):
+
+        res = {}
+    
+        res['phase_min'] = 0.
+        res['phase_max'] = 0.
+        for band in self.bands:
+            res['N_bef_'+band] = 0
+            res['N_aft_'+band] = 0
+            
+        if len(lc) == 0:
+            return res
+        
+        phases = (lc['time']-T0)/(1.+z)
+        res['phase_min'] = np.min(phases)
+        res['phase_max'] = np.max(phases)
+        
+        for band in self.bands:
+            idx = lc['band'] == 'LSST::'+band
+            sel = lc[idx]
+            if len(sel) > 0:
+                diff = sel['time']-T0
+                idb = diff >=0
+                selb = sel[idb]
+                res['N_bef_'+band] = len(sel)-len(selb)
+                res['N_aft_'+band] = len(selb)
+                
+            else:
+                res['N_bef_'+band] = 0
+                res['N_aft_'+band] = 0
+                
+        return res
--- SN_Fit/sn_fit.py
+++ SN_Fit/sn_fit.py
@@ -7,6 +7,47 @@ from importlib import import_module
 from SN_Telescope import Telescope
 import os
 import numpy as np
+import multiprocessing
+
+def Multiproc(simu_name='', lc_name='',Fit=None,nproc=1):
+
+    f = h5py.File(simu_name, 'r')
+    print(f.keys())
+    for i, key in enumerate(f.keys()):
+        simu = Table.read(simu_name, path=key)
+
+    nlc = len(simu)
+    print('total number of LC',nlc)
+    nlc = 32
+    batch = range(0,nlc,nproc)
+    batch = np.append(batch,nlc)
+    
+    names = None
+    val =[]
+    for i in range(len(batch)-1):
+        result_queue = multiprocessing.Queue()
+        
+        ida = batch[i]
+        idb = batch[i+1]
+    
+        for j in range(ida,idb):
+            lc =  Table.read(lc_name, path='lc_'+str(simu[ida]['id_hdf5']))
+            p=multiprocessing.Process(name='Subprocess-'+str(j),target=Fit,args=(lc,j,result_queue))
+            p.start()
+
+        
+        resultdict = {}
+        for j in range(ida,idb):
+            resultdict.update(result_queue.get())
+        
+        for p in multiprocessing.active_children():
+            p.join()
+
+        for j in range(ida,idb):
+            names = resultdict[j][0]
+            val.append(tuple(resultdict[j][1]))
+            
+    Fit.Finish(names,val)
 
 class Fit_All:
     def __init__(self,telescope,output_config,display_lc,fitter_config,prodid):
@@ -19,13 +60,13 @@ class Fit_All:
 
         self.val =[]
     
-    def __call__(self,lc):
+    def __call__(self,lc,j=-1,output_q=None):
         
         names,val = self.fitter(lc)
 
-        self.names = names
-        self.val.append(tuple(val))
-
+        if output_q is not None:
+            output_q.put({j : (list(names),list(val))})
+            
     def _prepare_Save(self, outdir, prodid):
 
         if not os.path.exists(outdir):
@@ -36,11 +77,11 @@ class Fit_All:
         if os.path.exists(self.fit_out):
             os.remove(self.fit_out)
             
-    def Finish(self):
+    def Finish(self,names,val):
         
         # dump the results in a hdf5 file
-        Table(rows = self.val, names=self.names).write(self.fit_out, 'fit_lc', compression=True)
-
+        Table(rows = val, names=names).write(self.fit_out, 'fit_lc', compression=True)
+        
         
 parser = argparse.ArgumentParser(
     description='Run a LC fitter from a configuration file')
@@ -69,23 +110,8 @@ def run(config_filename):
     Fit = Fit_All(telescope,config['Output'],config['Display'],config['Fitter'],prodid)
 
     # Loop on the simu_file to grab simulated LCs
-    
-    f = h5py.File(simu_name, 'r')
-    print(f.keys())
-    for i, key in enumerate(f.keys()):
-        simu = Table.read(simu_name, path=key)
-        print(len(simu), simu.dtype)
-        for ko,info in enumerate(simu):
-            print(info)
-            if info['n_lc_points'] > 0:
-                print(info['n_lc_points'],info['id_hdf5'])
-                lc =  Table.read(lc_name, path='lc_'+str(info['id_hdf5']))
-                Fit(lc)
-
-            if ko > 4:
-                break
-
-    Fit.Finish()
+    Multiproc(simu_name=simu_name,lc_name=lc_name,Fit=Fit,nproc=config['Multiprocessing']['nproc'])
+  
     
 def main(args):
     print('running')
