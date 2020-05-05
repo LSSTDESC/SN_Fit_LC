ca65180d64cf55c1f0f7de78754abb1846457df8
Philippe Gris <philippe.gris@clermont.in2p3.fr>
2020-05-05 15:35:19 +0200

    code cleaning plus docstring

--- sn_fitter/sn_Cosmo.py
+++ sn_fitter/sn_Cosmo.py
@@ -1,55 +1,104 @@
 import sncosmo
 import numpy as np
 from astropy import (cosmology, units as u, constants as const)
+import pandas as pd
+
 
 class Fit_LC:
-    def __init__(self,model='salt2-extended',version=1.0,telescope=None,display=False,bands='ugrizy'):
-      
+    """
+    class to fit simulated light curves
+
+    Parameters
+    ---------------
+    model: str, opt
+      model to use for the fit (default: salt2-extended)
+    version: float, opt
+       model version (default: 1.0)
+    telescope: sn_tools.telescope,opt
+      telescope model (default: None)
+    display: bool, opt
+      to display the fit online (default: False)
+    bands: str, opt
+      bands to consider (default: ugrizy)
+
+    """
+
+    def __init__(self, model='salt2-extended', version=1.0, telescope=None, display=False, bands='ugrizy'):
+
         self.display = display
         self.bands = bands
 
+        # get the bands for sncosmo registration - with and without airmass
+
         for band in bands:
             if telescope.airmass > 0:
-                band=sncosmo.Bandpass(telescope.atmosphere[band].wavelen,telescope.atmosphere[band].sb, name='LSST::'+band,wave_unit=u.nm)
+                band = sncosmo.Bandpass(
+                    telescope.atmosphere[band].wavelen, telescope.atmosphere[band].sb, name='LSST::'+band, wave_unit=u.nm)
             else:
-                band=sncosmo.Bandpass(telescope.system[band].wavelen,telescope.system[band].sb, name='LSST::'+band,wave_unit=u.nm) 
+                band = sncosmo.Bandpass(
+                    telescope.system[band].wavelen, telescope.system[band].sb, name='LSST::'+band, wave_unit=u.nm)
             sncosmo.registry.register(band, force=True)
 
-        source=sncosmo.get_source(model,version=str(version))
+        # get the source
+        source = sncosmo.get_source(model, version=str(version))
+        # get the dust
         dust = sncosmo.OD94Dust()
 
-        self.SN_fit_model=sncosmo.Model(source=source)
-        self.parNames = dict(zip(['z', 't0', 'x0', 'x1', 'c'],['z', 't0', 'x0', 'x1', 'color']))
+        # sn_fit_model instance
+        self.SN_fit_model = sncosmo.Model(source=source)
 
+        # name parameters
+        self.parNames = dict(zip(['z', 't0', 'x0', 'x1', 'c'], [
+                             'z', 't0', 'x0', 'x1', 'color']))
 
-    def __call__(self,lc):
+    def __call__(self, lc):
+        """
+        call method: this is where the fit is effectively performed
+
+        Parameters
+        ---------------
+        lc: astropy table
+           lc points
+
+        Returns
+        -----------
+
+        """
 
         if lc is None:
             return None, None
-        
+
         res_param_names = ['z', 't0', 'x0', 'x1', 'c']
         res_params_values = np.zeros((5,), dtype=float)
         vparam_names = ['t0', 'x0', 'x1', 'c']
-        covariance = np.zeros((4,4,), dtype=float)
+        covariance = np.zeros((4, 4,), dtype=float)
         mbfit = -1
         z = -1
         fitstatus = 'nofit'
         meta = None
         if lc is not None:
+            # get metadata
             meta = lc.meta
+
+            # set redshift for the fit
             z = meta['z']
             self.SN_fit_model.set(z=z)
-        
-            print('redshift',z)
-            selecta=lc[np.where(np.logical_and(lc['flux']/lc['fluxerr']>5.,lc['flux']>0.))]
-            selecta = selecta[['flux','fluxerr','band','zp','zpsys','time']]
-            select = lc[['flux','fluxerr','band','zp','zpsys','time']]
+
+            # LC points selection: make sure enough points with minimal snr
+
+            selecta = lc[np.where(np.logical_and(
+                lc['flux']/lc['fluxerr'] > 5., lc['flux'] > 0.))]
+            selecta = selecta[['flux', 'fluxerr',
+                               'band', 'zp', 'zpsys', 'time']]
+            select = lc[['flux', 'fluxerr', 'band', 'zp', 'zpsys', 'time']]
+            select = select[select['flux'] > 0.]
             if len(selecta) >= 5:
                 try:
-                    #print(select.dtype)
-                    #print('there man',len(select),select['flux']/select['fluxerr'])
-                    res, fitted_model = sncosmo.fit_lc(select, self.SN_fit_model,['t0', 'x0', 'x1', 'c'],bounds={'z':(z-0.001, z+0.001)},min_snr=0.)
-                    mbfit=fitted_model._source.peakmag('bessellb','vega')
+                    # fit here
+                    res, fitted_model = sncosmo.fit_lc(select, self.SN_fit_model, [
+                        't0', 'x0', 'x1', 'c'], bounds={'z': (z-0.001, z+0.001)}, minsnr=0.0)
+                    # get parameters
+                    mbfit = fitted_model._source.peakmag('bessellb', 'vega')
                     res_param_names = res['param_names']
                     res_params_values = res['parameters']
                     vparam_names = res['vparam_names']
@@ -59,87 +108,155 @@ class Fit_LC:
                     fitstatus = 'crash'
             else:
                 fitstatus = 'nodat'
-                
-        resa = self._transform(meta,res_param_names, list(res_params_values), vparam_names,covariance,mbfit,fitstatus)
-        resb = self._get_infos(z,res_params_values[res_param_names.index('t0')],select)
+
+       # Make a dict of the fitted result (plus metadata)
+        resa = self._transform(meta, res_param_names, list(
+            res_params_values), vparam_names, covariance, mbfit, fitstatus)
+
+        resb = self._get_infos(
+            z, res_params_values[res_param_names.index('t0')], select)
 
         if self.display and len(select) >= 5:
             import pylab as plt
-            sncosmo.plot_lc(select, model=fitted_model,color='r',pulls=False,errors=res.errors) 
+            sncosmo.plot_lc(select, model=fitted_model,
+                            color='r', pulls=False, errors=res.errors)
             plt.show()
-            
+
         resa.update(resb)
-        
+
         return resa.keys(), resa.values()
-    
-    def _transform(self,meta,par_names,params,vpar_names,covmat,mbfit,fitstatus):
-        
+
+    def _transform(self, meta, par_names, params, vpar_names, covmat, mbfit, fitstatus):
+        """
+        Method to transform input data to a coherent dictionary
+
+        Parameters
+        ---------------
+        meta: dict
+          LC metadata
+        par_names: list(str)
+          parameter list
+        params: list(float)
+          parameter values
+        vpar_names: list(str)
+          covariance parameter list
+        covmat: matrix
+           covariance matrix
+        mbfit: float
+          mbfit value
+        fitstatus: str
+           status of the fit
+
+        Returns
+        ----------
+        dict with the following keys
+        - SN parameters (from metadata):
+           Dec, Ra, SNID, color, dL, daymax, epsilon_color, epsilon_daymax, epsilon_x0, epsilon_x1,
+           index_hdf5, pixDec, pixID, pixRa, season, survey_area, x0, x1, z
+        - result of the fit:
+          z_fit, t0_fit, x0_fit, x1_fit, color_fit, Cov_t0t0, Cov_t0x0, Cov_t0x1, Cov_t0color, Cov_x0x0,
+         Cov_x0x1, Cov_x0color, Cov_x1x1, Cov_x1color, Cov_colorcolor, mbfit, fitstatus
+        """
         res = {}
-        for key, value in meta.items(): 
-            res[key]=value
+        for key, value in meta.items():
+            res[key] = value
 
         for i in range(len(par_names)):
-            res[self.parNames[par_names[i]]+'_fit']=params[i]
+            res[self.parNames[par_names[i]]+'_fit'] = params[i]
 
         for i, name in enumerate(vpar_names):
             for j, nameb in enumerate(vpar_names):
                 if j >= i:
-                    res['Cov_'+self.parNames[name]+self.parNames[nameb]] = covmat[i,j]
-                    
+                    res['Cov_'+self.parNames[name] +
+                        self.parNames[nameb]] = covmat[i, j]
+
         res['mbfit'] = mbfit
         res['fitstatus'] = fitstatus
         return res
 
     def _get_infos(self, z, T0, lc):
+        """
+        Method to estimate LC features
 
-        res = {}
-    
-        res['phase_min'] = 0.
-        res['phase_max'] = 0.
-        for band in self.bands:
-            res['N_bef_'+band] = 0
-            res['N_aft_'+band] = 0
-            res['SNR_'+band] = 0.
-            
-        res['N_bef_all'] = 0
-        res['N_aft_all'] = 0
-        
-        if len(lc) == 0:
-            return res
-        
-        phases = (lc['time']-T0)/(1.+z)
-        res['phase_min'] = np.min(phases)
-        res['phase_max'] = np.max(phases)
+        Parameters
+        ---------------
+        z: float
+          redshift value
+        T0:  float
+          daymax value
+        lc: astropy table
+            LC points
 
-        nbef = 0
-        naft = 0
-        for band in self.bands:
-            idx = lc['band'] == 'LSST::'+band
-            sel = lc[idx]
+        Returns
+        -----------
+        dict with the following keys:
+        phase_min, phase_max, N_bef, N_aft, N_bef_u, N_aft_u,
+        SNR_u, N_bef_g, N_aft_g, SNR_g, N_bef_r, N_aft_r, SNR_r,
+        N_bef_i, N_aft_i, SNR_i, N_bef_z, N_aft_z, SNR_z, N_bef_y,
+        N_aft_y, SNR_y
+
+        """
+
+        lcdf = lc.to_pandas()
+
+        lcdf['phase'] = (lcdf['time']-T0)/(1.+z)
+
+        lcdf['N_bef'] = lcdf['phase'] <= 0
+        lcdf['N_aft'] = lcdf['phase'] > 0
+
+        resdf = lcdf.groupby(['band']).apply(
+            lambda x: self.calc(x)).reset_index()
+
+        resfi = pd.DataFrame([lcdf['phase'].min()], columns=['phase_min'])
+        resfi['phase_max'] = lcdf['phase'].max()
+        resfi['N_bef'] = lcdf['N_bef'].sum()
+        resfi['N_aft'] = lcdf['N_aft'].sum()
+
+        for b in self.bands:
+            idx = resdf['band'] == 'LSST::'+b
+            sel = resdf[idx]
             if len(sel) > 0:
-                diff = sel['time']-T0
-                idb = diff >=0
-                selb = sel[idb]
-                res['N_bef_'+band] = len(sel)-len(selb)
-                res['N_aft_'+band] = len(selb)
-                nbef +=  len(sel)-len(selb)
-                naft += len(selb)
-                res['SNR_'+band] = self._calcSNR(sel)
+                for var in ['N_bef', 'N_aft', 'SNR']:
+                    resfi['{}_{}'.format(var, b)] = sel[var].values
             else:
-                res['N_bef_'+band] = 0
-                res['N_aft_'+band] = 0
-                res['SNR_'+band] = 0.
-        res['N_bef_all'] = nbef
-        res['N_aft_all'] = naft
-        
-        return res
+                for var in ['N_bef', 'N_aft', 'SNR']:
+                    resfi['{}_{}'.format(var, b)] = 0
 
-    def _calcSNR(self,lc):
-        
+        return resfi.to_dict(orient='record')[0]
+
+    def SNR(self, lc):
+        """
+        Method to estimate SNR of lc
+
+        Parameters
+        ---------------
+        lc: pandas df
+           data to process
+
+        Returns
+        -----------
+        Signal-to-Noise Ratio (SNR) defined by
+        SNR = sqrt(sum(flux/fluxerr)**2)
+
+        """
         idx = lc['flux']/lc['fluxerr'] > 0.
         sel = lc[idx]
-     
-        sum_flux = np.sum(sel['flux'])
-        rms_flux = np.sum(sel['fluxerr']**2)
 
-        return sum_flux/np.sqrt(rms_flux)
+        return np.sqrt(np.sum((sel['flux']/sel['fluxerr'])**2))
+
+    def calc(self, grp):
+        """
+        Method to estimate a set of infos on groups
+
+        Parameters
+        --------------
+        grp: pandas df group
+          data to process
+
+        Returns
+        ----------
+
+        """
+        return pd.DataFrame({'N_bef': [grp['N_bef'].sum()],
+                             'N_aft': [grp['N_aft'].sum()],
+                             'SNR': [self.SNR(grp)]})
