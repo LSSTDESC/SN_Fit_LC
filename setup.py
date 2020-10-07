1b8c8f73ea282be470154e0861afb799e0b2b7a6
Philippe Gris <gris@clermont.in2p3.fr>
2020-10-07 17:39:39 +0200

    modified setup to install sn_fit_input in addition to existing pcks of sn_fit_lc

--- setup.py
+++ setup.py
@@ -1,15 +1,20 @@
 from setuptools import setup
 
+# get the version here
+pkg_vars  = {}
+
+with open("version.py") as fp:
+    exec(fp.read(), pkg_vars)
 
 setup(
     name='sn_fit_lc',
-    version='v1.0.0',
+    version=pkg_vars['__version__'],
     description='Light curve fitting for supernovae',
     url='http://github.com/lsstdesc/sn_fit_lc',
     author='Philippe Gris',
     author_email='philippe.gris@clermont.in2p3.fr',
     license='BSD',
-    packages=['sn_fit', 'sn_fitter'],
+    packages=['sn_fit', 'sn_fitter','sn_fit_input'],
     python_requires='>=3.5',
     zip_safe=False,
     install_requires=[
