e9822b38737b8f107e5a565a184b5e3c38008775
Philippe Gris <philippe.gris@clermont.in2p3.fr>
2020-10-16 13:38:45 +0200

    modified setup.py to install txt files in sn_fit_input

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 # get the version here
-pkg_vars  = {}
+pkg_vars = {}
 
 with open("version.py") as fp:
     exec(fp.read(), pkg_vars)
@@ -14,7 +14,9 @@ setup(
     author='Philippe Gris',
     author_email='philippe.gris@clermont.in2p3.fr',
     license='BSD',
-    packages=['sn_fit', 'sn_fitter','sn_fit_input'],
+    packages=['sn_fit', 'sn_fitter', 'sn_fit_input'],
+    # All files from folder sn_fit_input
+    package_data={'sn_fit_input': ['*.txt']},
     python_requires='>=3.5',
     zip_safe=False,
     install_requires=[
