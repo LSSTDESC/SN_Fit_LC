42f2ec65af3240aea194caa93122f03ce6c72c29
Philippe Gris <gris@clermont.in2p3.fr>
2021-02-08 15:09:37 +0100

    implement an error model flux selection on r and g bands

--- sn_fit/sn_utils.py
+++ sn_fit/sn_utils.py
@@ -69,9 +69,8 @@ class Selection:
 
         # remove points with too high errormodel
         if self.errmodrel > 0.:
-            idx = selecta['fluxerr_model']/selecta['flux'] <= self.errmodrel
-            selecta = selecta[idx]
-
+            selecta = self.select_error_model(selecta)
+        
         # select LC points according to SNRmin
         idx = selecta['snr'] >= self.snrmin
         selecta = selecta[idx]
@@ -122,3 +121,71 @@ class Selection:
                 return None
 
         return selecta
+
+    def select_error_model(self, lc):
+        """
+        function to select LCs
+
+        Parameters
+        ---------------
+        lc: astropy table
+          lc to consider
+
+        Returns
+        ----------
+        lc with filtered values
+       """
+
+        if self.errmodrel < 0.:
+            return lc
+
+        # first: select iyz bands
+
+        bands_to_keep = []
+
+        lc_sel = Table()
+        for b in 'izy':
+            bands_to_keep.append('LSST::{}'.format(b))
+            idx = lc['band'] == 'LSST::{}'.format(b)
+            lc_sel = vstack([lc_sel, lc[idx]])
+
+        # now apply selection on g band for z>=0.25
+        sel_g = self.sel_band(lc, 'g', 0.25)
+
+        # now apply selection on r band for z>=0.6
+        sel_r = self.sel_band(lc, 'r', 0.6)
+
+        lc_sel = vstack([lc_sel, sel_g])
+        lc_sel = vstack([lc_sel, sel_r])
+
+        return lc_sel
+
+    def sel_band(self, tab, b, zref):
+        """
+        Method to performe selections depending on the band and z
+
+        Parameters
+        ---------------
+        tab: astropy table
+          lc to process
+        b: str
+          band to consider
+        zref: float
+           redshift below wiwh the cut wwill be applied
+
+        Returns
+        ----------
+        selected lc
+        """
+
+        idx = tab['band'] == 'LSST::{}'.format(b)
+        sel = tab[idx]
+        if len(sel) == 0:
+            return Table()
+
+        if sel.meta['z'] >= zref:
+            idb = sel['fluxerr_model']/sel['flux'] <= self.errmodrel
+            selb = sel[idb]
+            return selb
+
+        return sel
